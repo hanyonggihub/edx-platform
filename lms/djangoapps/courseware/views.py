675368e72776967f542321bd1cf98998a508903a
aamir-khan <aamir@edx.org>
2015-12-12 00:38:46 +0500

    ECOM-3107: Updated the comments

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -1010,7 +1010,7 @@ def _credit_course_requirements(course_key, student):
     if not (settings.FEATURES.get("ENABLE_CREDIT_ELIGIBILITY", False) and is_credit_course(course_key)):
         return None
 
-    # If student is not enrolled as a credit mode, short-circuit and return None. This indicates that
+    # If student is enrolled as a honor mode, short-circuit and return None. This indicates that
     # credit requirements should NOT be displayed on the progress page.
     enrollment = CourseEnrollment.get_enrollment(student, course_key)
     if enrollment.mode == CourseMode.HONOR:
